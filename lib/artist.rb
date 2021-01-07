e09c1f1f004bb2e6d220f1a8aba7de67757f4484
Danny Brandt <dvbrandt90@gmail.com>
2021-01-07 20:46:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,23 +5,40 @@ class Artist
     @name = name
     @songs = []
   end
+<<<<<<< HEAD
      
   def songs
     Song.all
   end
   
+=======
+    
+>>>>>>> 7308d7bce6f99bf21b4bce4ced25fed3a7e8d893
   def add_song(song)
     song.artist = self
     @songs << song
   end
   
+<<<<<<< HEAD
+=======
+  def songs 
+    @songs
+  end
+  
+>>>>>>> 7308d7bce6f99bf21b4bce4ced25fed3a7e8d893
   def add_song_by_name(name)
     song = Song.new(name)
     self.add_song(song)
   end
   
+<<<<<<< HEAD
   def self.song_count
     Song.all.count
   end
   
+=======
+  def song_count
+   count =  @songs.count
+  end
+>>>>>>> 7308d7bce6f99bf21b4bce4ced25fed3a7e8d893
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,19 @@
 class Song
   attr_accessor :name, :artist
   
+<<<<<<< HEAD
   @@all = []
   
   def initialize(name)
     @name = name
     @@all << self
     
+=======
+  def initialize(name)
+    @name = name
+    @@all = []
+    @@all << self
+>>>>>>> 7308d7bce6f99bf21b4bce4ced25fed3a7e8d893
   end
   
   def self.all
